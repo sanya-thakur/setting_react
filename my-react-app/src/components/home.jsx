44816c007e2dc8a2e80c9f44387beff8083a9624
kalvium <sanya.thakur@kalvium.community>
2025-03-05 11:28:04 +0530

    done

---
+++ my-react-app/src/components/home.jsx
@@ -0,0 +1,9 @@
+const Home=()=>{
+    return(<div style={{textAlign: "center", margin: "50px"}}>
+        <h1>Contact Information</h1>
+        <h2>Sanya Thakur</h2>
+        <p>Email: sanya.thakur.s69@kalvium.community</p>
+    </div>);
+};
+
+export default Home;
